6688b7366b6abd0a1632e2cf41f46953e50ed9b1
Luke Korth <luke@lukekorth.com>
2012-05-09 14:20:41 -0500

    Rename and move logo image

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $title = "Mount Vernon Historic Preservation Commision";
 include'header.php';
 ?>
 <div style="width:100px; float:left; padding: 8px">
-<img src="assets/MVHPCLogo.png"/>
+<img src="images/MVHPC-Logo.png"/>
 </div>
 <div style="width:645px; float:right;">
 <?php
