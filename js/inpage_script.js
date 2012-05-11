4f1dbfc6334043d6dd1cf3b40bfbf1ff40c116bc
Luke Korth <luke@lukekorth.com>
2012-05-10 20:28:42 -0500

    Remove duplicate script and reformat

--- js/inpage_script.js
+++ js/inpage_script.js
@@ -1,21 +1,19 @@
 $().ready(function() {
-	$("#global").autocomplete("shared/suggest.php", {
-		width: 200,		
-		max: 8,		
-		scroll: false,		
-		selectFirst: false,
-		delay: 150,
-		scrollHeight: 500,		
-		});
+    $("#global").autocomplete("shared/suggest.php", {
+        width: 200,		
+        max: 8,		
+        scroll: false,		
+        selectFirst: false,
+        delay: 150,
+        scrollHeight: 500		
+    });
 		
-	$("#change").autocomplete("shared/suggest.php", {
-		width: 200,		
-		max: 8,		
-		scroll: false,		
-		selectFirst: false,
-		delay: 150,
-		scrollHeight: 500,		
-		});	
-		
-		
-		});
+    $("#change").autocomplete("shared/suggest.php", {
+        width: 200,		
+        max: 8,		
+        scroll: false,		
+        selectFirst: false,
+        delay: 150,
+        scrollHeight: 500		
+    });			
+});
--- js/inpage_script_1.js
+++
@@ -1,9 +0,0 @@
-$().ready(function() {
-	$("#global").autocomplete("suggest.php", {
-		width: 200,		
-		max: 8,		
-		scroll: false,		
-		selectFirst: false,
-		delay: 150,
-		scrollHeight: 500,		
-		});});
