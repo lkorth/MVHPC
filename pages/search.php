0041c0ac4b8e45c55ec94697bc74353291b818eb
Luke Korth <luke@lukekorth.com>
2012-05-16 23:04:37 -0500

    Refactor search page using pager class to simplify logic

--- pages/search.php
+++ pages/search.php
@@ -1,100 +1,85 @@
 <?php
 
-if ($_GET['changetags'] != null) {
-    $pagetype = "change_tags.php?";
+if (!empty($_GET['changetags'])) {
+    $nextPage = "change_tags.php?";
     $cgtags = 1;
 } else {
-    $pagetype = "fullsize.php?";
+    $nextPage = "fullsize.php?";
     $cgtags = null;
 }
 
 $page = $_GET['page'];
-$terms = $_GET['terms'];
-
+$terms = strtolower(urldecode($_GET['terms']));
 $title = "Search of " . $terms;
-$terms = strtolower($terms);
 
 $level = '../';
 include '../shared/header.php';
 echo "<br>";
 
 $db = Database::getDatabase();
-
-$result = $db->query("SELECT thumbnail,id MATCH(tags, information) AGAINST('$terms') AS score FROM search WHERE MATCH(tags, information) AGAINST('$terms') ORDER BY score DESC, views DESC");
+$result = $db->query("SELECT thumbnail,id, MATCH(tags, information) AGAINST('$terms') AS score FROM search WHERE MATCH(tags, information) AGAINST('$terms') ORDER BY score DESC, views DESC");
 $num = $db->numRows($result) ? $db->numRows($result) : 0;
 
-if ($num <= 25) {
-    $i = 0;
-}
-else if ($page == 1 && $num > 25) {
-    $i = 0;
-} else if ($page > 1 && $num > 25) {
-    $i = (($page - 1) * 25);
-}
-
 if ($num == 0) {
-    $num = '';
     $result = $db->query("SELECT thumbnail,id FROM search WHERE tags LIKE '%$terms%' OR information LIKE '%$terms%' ORDER BY views DESC");
     $num = $db->numRows($result) ? $db->numRows($result) : 0;
     if ($num == 0) {
         echo "<p>No results found for the search \"" . $terms . "\".&nbsp;&nbsp;Please try a different search.</p>";
-    } else if ($num <= 25) {
-        $i = 0;
-    } else if ($page == 1 && $num > 25) {
-        $i = 0;
-    } else if ($page > 1 && $num > 25) {
-        $i = (($page - 1) * 25);
-    }
-    if ($num != 0) {
-        echo "<p>Your search returned " . $num . " results</p><br>";
-    }
-    
-    $i24 = $i + 25;
-    while ($i < $num && $i < $i24) {
-        $thumbnail = mysql_result($result, $i, "thumbnail");
-        $id = mysql_result($result, $i, "id");
-        echo "<a href=\"$pagetype" . "id=$id\"><img src=\"$thumbnail\"/></a>&nbsp;";
-        $i++;
-    }
-} else {
-    $i24 = "";
-    $i24 = $i + 25;
-    while ($i < $num && $i < $i24) {
-        $thumbnail = mysql_result($result, $i, "thumbnail");
-        $id = mysql_result($result, $i, "id");
-        echo "<a href=\"$pagetype" . "id=$id\"><img src=\"$thumbnail\"/></a>&nbsp;";
-        $i++;
+        exit;
     }
 }
 
-$needed = ($num / 25);
-$needed = ceil($needed);
+echo "<p>Your search returned " . $num . " results</p><br>";
 
-if ($needed > 1) {
-    $next = $page + 1;
-    $space = str_replace(" ", "+", $terms);
-    echo "<div>";
-    echo "<br>";
-    if ($page > 1) {
-        $pre = $page - 1;
-        echo "<a href=\"search.php?page=$pre&terms=$space&changetags=$cgtags\"/><=Prevous Page=</a>";
+$pager = new Pager($page, 25, $num);
+$pager->calculate();
+
+for ($i = $pager->firstRecord; $i <= $pager->lastRecord; $i++) {
+    $thumbnail = mysql_result($result, $i, "thumbnail");
+    $id = mysql_result($result, $i, "id");
+    echo "<a href=\"$nextPage" . "id=$id\"><img src=\"$thumbnail\"/></a>&nbsp;";
+}
+
+if ($pager->hasNextPage() || $pager->hasPrevPage()) {
+    $encodedTerms = urlencode($terms);
+    echo '<div><br>';
+
+    if ($pager->hasPrevPage()) {
+        echo '<a href="search.php?page=' . $pager->prevPage() . '&terms=' . $encodedTerms . '&changetags=' . $cgtags . '"/><=Prevous Page=</a>';
         echo "&nbsp;&nbsp;&nbsp;";
     }
+
+    if ($pager->prevPage() > 1) {
+        $previousPageNumbers = '';
+        $j = $pager->prevPage();
+        $previousCount = 0;
+        while ($j >= 1 && $previousCount < 3) {
+            $previousPageNumbers = '<a href="search.php?page=' . $j . '&terms=' . $encodedTerms . '&changetags=' . $cgtags . '"/>' . $j . '</a>&nbsp;&nbsp;&nbsp;' . $previousPageNumbers;
+            $previousCount++;
+            $j--;
+        }
+        echo $previousPageNumbers;
+    }
+
     echo "<span style=\"color:white\">$page</span>";
     echo "&nbsp;&nbsp;&nbsp;";
-    $pageindex = $page + 1;
-    for ($i = 0; $i < 5; $i++) {
-        if ($pageindex <= $needed) {
-            echo "<a href=\"search.php?page=$pageindex&terms=$space&changetags=$cgtags\"/>$pageindex</a>";
-            echo "&nbsp;&nbsp;&nbsp;";
-            $pageindex++;
+
+    if ($pager->hasNextPage()) {
+        if ($pager->nextPage() != $pager->numPages) {
+            $k = $pager->nextPage();
+            $nextCount = 0;
+            while ($k <= $pager->numPages && $nextCount < 3) {
+                echo '<a href="search.php?page=' . $k . '&terms=' . $encodedTerms . '&changetags=' . $cgtags . '"/>' . $k . '</a>&nbsp;&nbsp;&nbsp;';
+                $nextCount++;
+                $k++;
+            }
         }
+
+        echo '<a href="search.php?page=' . $pager->nextPage() . '&terms=' . $encodedTerms . '&changetags=' . $cgtags . '"/>=Next Page=></a>';
     }
-    if ($page < $needed) {
-        echo "<a href=\"search.php?page=$next&terms=$space&changetags=$cgtags\"/>=Next Page=></a>";
-    }
-    echo "<br>";
-    echo "</div>";
+
+    echo '<br></div>';
 }
+
 include '../shared/footer.php';
 ?>
