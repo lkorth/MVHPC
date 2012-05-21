065521d7ff26dd2bdb29de87e4d61b5ac1e70635
Luke Korth <luke@lukekorth.com>
2012-05-21 14:12:38 -0500

    Temp fix for path

--- js/script.js
+++ js/script.js
@@ -11,28 +11,28 @@
 
 window.addEvent('domready', function() { // wait for the content
 
-    // our uploader instance 
-	
+    // our uploader instance
+
     var up = new FancyUpload2($('demo-status'), $('demo-list'), { // options object
         // url is read from the form, so you just have to change one place
         url: $('form-demo').action,
-		
+
         // path to the SWF file
-        path: 'flash/Swiff.Uploader.swf',
-		
+        path: '/mvhpc/flash/Swiff.Uploader.swf',
+
         // remove that line to select all files, or edit it, add more items
         typeFilter: {
             'Media: (*.jpg, *.jpeg, *.gif, *.png)': '*.jpg; *.jpeg; *.gif; *.png'
         },
-		
+
         // this is our browse button, *target* is overlayed with the Flash movie
         target: 'demo-browse',
-		
+
         // graceful degradation, onLoad is only called if all went well with Flash
         onLoad: function() {
             $('demo-status').removeClass('hide'); // we show the actual UI
             $('demo-fallback').destroy(); // ... and hide the plain form
-			
+
             // We relay the interactions with the overlayed flash to the link
             this.target.addEvents({
                 click: function() {
@@ -51,7 +51,7 @@ window.addEvent('domready', function() { // wait for the content
             });
 
             // Interactions for the 2 other buttons
-			
+
             $('demo-clear').addEvent('click', function() {
                 up.remove(); // remove all files
                 return false;
@@ -62,15 +62,15 @@ window.addEvent('domready', function() { // wait for the content
                 return false;
             });
         },
-		
+
         // Edit the following lines, it is your custom event handling
-		
+
         /**
 		 * Is called when files were not added, "files" is an array of invalid File classes.
-		 * 
+		 *
 		 * This example creates a list of error elements directly in the file list, which
 		 * hide on click.
-		 */ 
+		 */
         onSelectFail: function(files) {
             files.each(function(file) {
                 new Element('li', {
@@ -85,7 +85,7 @@ window.addEvent('domready', function() { // wait for the content
                 }).inject(this.list, 'top');
             }, this);
         },
-		
+
         /**
 		 * This one was directly in FancyUpload2 before, the event makes it
 		 * easier for you, to add your own response handling (you probably want
@@ -93,7 +93,7 @@ window.addEvent('domready', function() { // wait for the content
 		 */
         onFileSuccess: function(file, response) {
             var json = new Hash(JSON.decode(response, true) || {});
-			
+
             if (json.get('status') == '1') {
                 file.element.addClass('file-success');
                 file.info.set('html', '<strong>Image was successfully uploaded:</strong> ' + json.get('width') + ' x ' + json.get('height') + 'px, <em>' + json.get('mime') + '</em>)');
@@ -102,7 +102,7 @@ window.addEvent('domready', function() { // wait for the content
                 file.info.set('html', '<strong>An error occured:</strong> ' + (json.get('error') ? (json.get('error') + ' #' + json.get('code')) : response));
             }
         },
-		
+
         /**
 		 * onFail is called when the Flash movie got bashed by some browser plugin
 		 * like Adblock or Flashblock.
@@ -122,8 +122,8 @@ window.addEvent('domready', function() { // wait for the content
                     alert('To enable the embedded uploader, install the latest Adobe Flash plugin.')
             }
         }
-		
+
     });
-	
+
 });
 		//]]>
