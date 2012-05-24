ee8ff6051b7afb79677410adf4b61b98aa3665f0
mckennapsean <mckennapsean@gmail.com>
2012-05-23 19:35:20 -0500

    refactor page controller for additions to come

--- pages/page-controller.php
+++ pages/page-controller.php
@@ -3,88 +3,89 @@
 require_once '../includes/master.inc.php';
 
 if (!isset($_GET['page']))
-    redirect('../error/404.php');
+  redirect('../error/404.php');
 
+// get page and subpage (if applicable)
 $page = $_GET['page'];
-
-if ($page == 'map') {
-    if (isset($_GET['subpage']) && !empty($_GET['subpage'])) {
-        $subpage = $_GET['subpage'];
-
-        if ($subpage == 'ash-park-district') {
-
-        } else if ($subpage == 'commercial-district') {
-
-        } else if ($subpage == 'cornell-district') {
-
-        }
-        else
-            redirect('../error/404.php');
-    }
-    else {
-        $title = "MVHPC :: Map of Mount Vernon";
-        $id = 0;
-        // Old district info
-        // $title = "MVHPC :: Districts";
-        // $id = 4;
-    }
-} else if ($page == 'archives') {
-    if (isset($_GET['subpage']) && !empty($_GET['subpage'])) {
-        $subpage = $_GET['subpage'];
-
-        if ($subpage == 'documents') {
-            $title = "MVHPC :: Documents";
-            $id = 5;
-        } else if ($subpage == 'images') {
-            $title = "MVHPC :: Images";
-            $id = 6;
-        }
-        else
-            redirect('../error/404.php');
-    }
-    else {
-        $title = "MVHPC :: Archives";
-        $id = 2;
-    }
-} else if ($page == 'about-us') {
-    if (isset($_GET['subpage']) && !empty($_GET['subpage'])) {
-        $subpage = $_GET['subpage'];
-
-        if ($subpage == 'links') {
-            $title = "MVHPC :: Links";
-            $id = 8;
-        } else if ($subpage == 'design-review') {
-            $title = "MVHPC :: Design Review";
-            $id = 7;
-        }
-        else
-            redirect('../error/404.php');
-    }
-    else {
-        $title = "MVHPC :: About Us";
-        $id = 3;
-    }
-} else {
-    redirect('../error/404.php');
+$subpage = NULL;
+if (isset($_GET['subpage']) && !empty($_GET['subpage'])) {
+  $subpage = $_GET['subpage'];
 }
 
-  $level = '../';
-  include '../shared/header.php';
+if ($page == 'map') {
+  if ($subpage == NULL) {
+      $title = 'MVHPC :: Map of Mount Vernon';
+      $id = 0;
 
-  // if $id exists, load page from database
-  if ($id != 0) {
+  } else if ($subpage == 'ash-park-district') {
+      
+
+  } else if ($subpage == 'commercial-district') {
+      
+
+  } else if ($subpage == 'cornell-district') {
+      
+
+  } else {
+      redirect('../error/404.php');
+  }
+
+} else if ($page == 'archives') {
+  if ($subpage == NULL) {
+      $title = 'MVHPC :: Archives';
+      $id = 2;
+
+  } else if ($subpage == 'documents') {
+      $title = 'MVHPC :: Documents';
+      $id = 0;
+
+  } else if ($subpage == 'images') {
+      $title = 'MVHPC :: Images';
+      $id = 6;
+
+  } else {
+      redirect('../error/404.php');
+  }
+
+} else if ($page == 'about-us') {
+  if ( $subpage == NULL) {
+      $title = 'MVHPC :: About Us';
+      $id = 3;
+
+  } else if ($subpage == 'links') {
+      $title = 'MVHPC :: Links';
+      $id = 8;
+  } else if ($subpage == 'design-review') {
+      $title = 'MVHPC :: Design Review';
+      $id = 7;
+  } else {
+      redirect('../error/404.php');
+  }
+
+} else {
+    redirect('error/404.php');
+}
+
+$level = '../';
+include '../shared/header.php';
+
+// if $id exists, load page from database
+if ($id != 0) {
   $db = Database::getDatabase();
   $row = $db->getRow("SELECT * FROM pages WHERE id = '$id'");
   echo $row['data'];
-  }
+}
 
-  // otherwise, load a file to display content
-  else {
+// otherwise, load a file to display content
+else {
   if ($page == 'map') {
-  include 'map.php';
-  }
-  }
+      include 'map.php';
 
-  include '../shared/footer.php';
+  } else if ($subpage == 'documents') {
+      include 'documents.php';
+  }
+}
+
+include '../shared/footer.php';
 
 ?>
