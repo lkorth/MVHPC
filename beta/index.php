8aa1ecbcfac7a447134879a61979ff7cade122b9
Adam Reed <areed12@cornellcollege.edu>
2012-05-23 22:06:03 -0500

    Template class done. Refactored index page to use two column template.

--- beta/index.php
+++ beta/index.php
@@ -5,76 +5,66 @@
 error_reporting('E_ALL');
 
 include('../includes/master.inc.php');
+include('../includes/class.template.php');
+
+$template = new Template();
 
 $posts = returnPosts('index', 2);
 
 ?>
-
 <html>
-    <head>
-        <title>Mount Vernon Historical Preservation Commission</title>
-        <link href='http://fonts.googleapis.com/css?family=Lobster+Two' rel='stylesheet' type='text/css' />
-        <link href='http://fonts.googleapis.com/css?family=Glass+Antiqua' rel='stylesheet' type='text/css' />
-        <link rel="stylesheet" type="text/css" href="stylesheets/compiled/main.css" />
-        <script src="/mvhpc/js/jquery-1.7.2.js"></script>
-        <script src="js/jquery.tagcloud.min.js"></script>
-        <script src="js/script.js"></script>
-    </head>
-    <body>
-        <div class="container">
-            <div class="header">
-
-            </div>
-            <div class="navbar">
-                <div class="menu">
-                    <span class="menu_wrapper">
-                        <a href='#'><span class="menu_item">Home</span></a>
-                        <a href='#'><span class="menu_item">Archives</span></a>
-                        <a href='#'><span class="menu_item">Map</span></a>
-                        <a href='#'><span class="menu_item">About</span></a>
-                    </span>
+<?php ob_start(); ?>
+    <script src="js/jquery.tagcloud.min.js"></script>
+<?php 
+    $headerExtras = ob_get_clean();
+    ob_start();
+?>
+    <div class="newsfeed">
+        <?php foreach($posts as $post): ?>
+        <h1 class="ribbon"><?php echo $post['title']; ?></h1>
+        <div class="article">
+            <p>
+                <?php echo max_words($post['text'], 150); ?>
+            </p>
+            <a href="<?php WEBROOT() ?>pages/posts.php?id=<?php echo $post['id']; ?>">Read more...</a>
+        </div>
+        <?php endforeach; ?>
+    </div>
+    <div class="left_box">
+        <div>
+            <h1 class="ribbon">Featured Images</h1>
+            <div class="left_content">
+                <?php $imgArr = featuredImages();
+                        $cnt = 0;
+                        foreach($imgArr as $img): ?>
+                        <?php if($cnt == 0): ?>
+                            <div class="featured current">
+                        <?php else: ?>
+                                <div class="featured">
+                        <?php endif; ?>
+                        <a href="<?php WEBROOT(); ?>pages/fullsize.php?id=<?php echo $img['id']; ?>"><img src="<?php echo WEB_ROOT . $img['thumbnail']; ?>" /></a>
+                    </div>
+                <?php
+                    $cnt++;
+                endforeach; ?>
                 </div>
+                <span style="display:block; margin:0 auto; color: #FF6600;"><button id="prevImg"><</button><button id="nextImg">></button></span>
             </div>
-            <div class="content">
-                    <div class="newsfeed">
-                        <?php foreach($posts as $post): ?>
-                        <h1 class="ribbon"><?php echo $post['title']; ?></h1>
-                        <div class="article">
-                            <p>
-                                <?php echo max_words($post['text'], 150); ?>
-                            </p>
-                            <a href="<?php WEBROOT() ?>pages/posts.php?id=<?php echo $post['id']; ?>">Read more...</a>
-                        </div>
-                        <?php endforeach; ?>
-                    </div>
-                <div class="left_box">
-                    <div>
-                        <h1 class="ribbon">Featured Images</h1>
-                        <div class="left_content">
-                        <?php $imgArr = featuredImages();
-                              $cnt = 0;
-                              foreach($imgArr as $img): ?>
-                                <?php if($cnt == 0): ?>
-                                    <div class="featured current">
-                                <?php else: ?>
-                                        <div class="featured">
-                                <?php endif; ?>
-                                <a href="<?php WEBROOT(); ?>pages/fullsize.php?id=<?php echo $img['id']; ?>"><img src="<?php echo WEB_ROOT . $img['thumbnail']; ?>" /></a>
-                            </div>
-                        <?php
-                            $cnt++;
-                        endforeach; ?>
-                        </div>
-                        <span style="display:block; margin:0 auto; color: #FF6600;"><button id="prevImg"><</button><button id="nextImg">></button></span>
-                    </div>
-                    <div>
-                        <h1 class="ribbon">Common Tags</h1>
-                        <div class="left_content" id="tagcloud">
-                            
-                        </div>
-                    </div>
+            <div>
+                <h1 class="ribbon">Common Tags</h1>
+                <div class="left_content" id="tagcloud">
+
                 </div>
             </div>
         </div>
-    </body>
-</html>
+    </div>
+<?php
+    $content = ob_get_clean();
+
+    $template->setStyle('twoColumn');
+    $template->setTitle('Mount Vernon Historial Preservation Commission');
+    $template->setHeaderExtras($headerExtras);
+    $template->setBody($content);
+
+    $template->output();
+?>
---
+++ includes/class.template.php
@@ -0,0 +1,80 @@
+<?php
+    class Template
+    {
+        private $DEFAULT_TITLE = "Mount Vernon Historical Preservation Commission";
+        private $DEFAULT_BODY = "There was an error finding the content for this page. Please contact the system administrator for assistance.";
+                
+        private $style;
+        private $title;
+        private $headerExtras;
+        private $body;
+        
+        public function _construct() {
+            $this->style = "oneColumn";
+            $this->title = $DEFAULT_TITLE;
+            $this->body = $DEFAULT_BODY;
+        }
+        
+        public function setStyle($style) {
+            $this->style = $style;
+        } 
+        
+        public function setTitle($title) {
+            $this->title = $title;
+        }
+        
+        public function setHeaderExtras($extras) {
+            $this->headerExtras = $extras;
+        }
+        
+        public function setBody($body) {
+            $this->body = $body;
+        }
+        
+        public function output() {
+            echo "<html>";
+            echo 
+                "
+                <head>
+                    <link href='http://fonts.googleapis.com/css?family=Lobster+Two' rel='stylesheet' type='text/css' />
+                    <link href='http://fonts.googleapis.com/css?family=Glass+Antiqua' rel='stylesheet' type='text/css' />
+                    <link rel='stylesheet' type='text/css' href='/mvhpc/beta/stylesheets/compiled/main.css' />
+                    <script src='/mvhpc/js/jquery-1.7.2.js'></script>
+                    <script src='/mvhpc/beta/js/script.js'></script>
+                ";
+            echo "<title>".$this->title."</title>";
+            echo $this->headerExtras;
+            echo "</head>";
+            
+            echo 
+                "
+                <body>
+                    
+                    <div class='container'>
+                        <div class='header'>
+                        </div>
+                        <div class='navbar'>
+                            <div class='menu'>
+                                <span class='menu_wrapper'>
+                                    <a href='#'><span class='menu_item'>Home</span></a>
+                                    <a href='#'><span class='menu_item'>Archives</span></a>
+                                    <a href='#'><span class='menu_item'>Map</span></a>
+                                    <a href='#'><span class='menu_item'>About</span></a>
+                                </span>
+                            </div>
+                        </div>
+                ";
+            if($this->style == "oneColumn"){
+                echo "<div class='content oneColumn'>";
+            }else if($this->style == "twoColumn"){
+                echo "<div class='content twoColumn'>";
+            }
+            echo $this->body;
+            echo "
+                        </div>
+                    </div>
+                </body>
+                ";
+            echo "</html>";
+        }   
+    }
