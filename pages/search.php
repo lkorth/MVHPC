b386f0bcfbad62d530bbbd43186a0be5c021c000
Luke Korth <luke@lukekorth.com>
2012-05-25 23:44:53 -0500

    Adapt search.php to template

--- pages/search.php
+++ pages/search.php
@@ -1,12 +1,15 @@
+<!doctype html>
+
 <?php
 
-$page = $_GET['page'];
-$terms = strtolower(urldecode($_GET['terms']));
-$title = "Search of " . $terms;
+include('../includes/master.inc.php');
+include('../includes/class.template.php');
 
-$level = '../';
-include '../shared/header.php';
-echo "<br>";
+$template = new Template();
+
+
+$terms = strtolower(urldecode($_GET['terms']));
+$page = (isset($_GET['page'])) ? $_GET['page'] : 1;
 
 $db = Database::getDatabase();
 $result = $db->query("SELECT thumbnail,id, MATCH(tags, information) AGAINST('$terms') AS score FROM search WHERE MATCH(tags, information) AGAINST('$terms') ORDER BY score DESC, views DESC");
@@ -17,22 +20,48 @@ if ($num == 0) {
     $num = $db->numRows($result) ? $db->numRows($result) : 0;
     if ($num == 0) {
         echo "<p>No results found for the search \"" . $terms . "\".&nbsp;&nbsp;Please try a different search.</p>";
-        exit;
     }
 }
 
-echo "<p>Your search returned " . $num . " results</p><br>";
+?>
+<html>
+<?php
+    $headerExtras['js'] = array('jquery-172.js',
+                                'jquery-bgiframe-min.js',
+                                'jquery-ajaxQueue.js',
+                                'jquery-autocomplete.js',
+                                'jquery-lightbox-05-min.js',
+                                'jquery-spellchecker.js',
+                                'jquery-tagsinput.js',
+                                'all-images.js');
+    $headerExtras['css'] = array('jquery-lightbox-05.css', 'jquery-spellchecker.css', 'jquery.autocomplete.css', 'jquery-tagsinput.css');
+?>
+<?php ob_start(); ?>
+    <?php
 
-$pager = new Pager($page, 16, $num);
-$pager->calculate();
+    echo "<p>Your search returned " . $num . " results</p><br>";
 
-for ($i = $pager->firstRecord; $i <= $pager->lastRecord; $i++) {
-    $thumbnail = mysql_result($result, $i, "thumbnail");
-    $id = mysql_result($result, $i, "id");
-    echo "<a href=\"fullsize.php" . "id=$id\"><img src=\"" . WEB_ROOT . "$thumbnail\"/></a>&nbsp;";
-}
+    $pager = new Pager($page, 16, $num);
+    $pager->calculate();
 
-renderPaging($pager, WEB_ROOT . 'pages/search.php?terms=' . $terms);
+    for ($i = $pager->firstRecord; $i <= $pager->lastRecord; $i++) {
+        $thumbnail = mysql_result($result, $i, "thumbnail");
+        $id = mysql_result($result, $i, "id");
+        echo "<a href=\"fullsize.php?" . "id=$id\"><img src=\"" . WEB_ROOT . "$thumbnail\"/></a>&nbsp;";
+    }
 
-include '../shared/footer.php';
+    renderPaging($pager, WEB_ROOT . 'pages/search.php?terms=' . $terms);
+
+    ?>
+    </div>
+
+<?php
+    $content = ob_get_clean();
+
+    $template->setStyle('oneColumn');
+    $template->setTitle('Search of ' . $terms);
+    $template->setHeaderExtras($headerExtras);
+    $template->setBody($content);
+
+    $template->output();
 ?>
