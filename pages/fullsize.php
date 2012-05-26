86543dd728822d1c5368726328e9ea0d8f2e3eba
Luke Korth <luke@lukekorth.com>
2012-05-25 23:50:31 -0500

    Adapt fullsize to template

--- pages/fullsize.php
+++ pages/fullsize.php
@@ -1,10 +1,13 @@
+<!doctype html>
+
 <?php
 
-$level = '../';
-include '../shared/header.php';
+include('../includes/master.inc.php');
+include('../includes/class.template.php');
 
-$title = "MVHPC.org";
-$zoom = 1;
+$template = new Template();
+
+
 $id = $_GET['id'];
 
 $db = Database::getDatabase();
@@ -18,14 +21,36 @@ $trueviews = $row['trueviews'] + 1;
 $views = $row['views'] + 0.05;
 $db->query("UPDATE search SET views = '$views', trueviews = '$trueviews' WHERE id = '$id'");
 
-include '../shared/header.php';
-echo "<br>";
-echo "<a href=\"$medlg\" class=\"MagicZoom\" rel=\"click-to-initialize:true;zoom-position:inner;zoom-fade:true;\"><img src=\"$mid\"/></a>";
-echo "<p>Click the picture to turn on zooming</p>";
-echo "<br><br>";
-echo '<p>' . $row['information'] . '</p>';
-echo "<br><br>";
-echo "<a href=\"$location\">Click here for full resolution picture (Warning some pictures are VERY large)</a><br>";
-echo "<a href=\"feedback.php?id=$id\">Click here to request tag update</a>";
-include '../shared/footer.php';
+?>
+<html>
+<?php
+    $headerExtras['js'] = array('jquery-172.js',
+                                'jquery-bgiframe-min.js',
+                                'jquery-ajaxQueue.js',
+                                'jquery-autocomplete.js',
+                                'jquery-lightbox-05-min.js',
+                                'jquery-spellchecker.js',
+                                'jquery-tagsinput.js',
+                                'all-images.js');
+    $headerExtras['css'] = array('jquery-lightbox-05.css', 'jquery-spellchecker.css', 'jquery.autocomplete.css', 'jquery-tagsinput.css');
+?>
+<?php ob_start(); ?>
+    <?php
+    echo "<br>";
+    echo "<a href=\"" . WEB_ROOT . "$medlg\" class=\"MagicZoom\" rel=\"click-to-initialize:true;zoom-position:inner;zoom-fade:true;\"><img src=\"" . WEB_ROOT . "$mid\"/></a>";
+    echo "<p>Click the picture to turn on zooming</p>";
+    echo "<br><br>";
+    echo '<p>' . $row['information'] . '</p>';
+    echo "<br><br>";
+    echo "<a href=\"" . WEB_ROOT . "$location\">Click here for full resolution picture (Warning some pictures are VERY large)</a><br>";
+    echo "<a href=\"feedback.php?id=$id\">Click here to request tag update</a>";
+    ?>
+<?php
+    $content = ob_get_clean();
+
+    $template->setStyle('oneColumn');
+    $template->setHeaderExtras($headerExtras);
+    $template->setBody($content);
+
+    $template->output();
 ?>
