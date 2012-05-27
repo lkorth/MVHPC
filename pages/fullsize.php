fe4a8d215f951023cf75b8631b0fc443715b52d2
mckennapsean <mckennapsean@gmail.com>
2012-05-27 16:37:56 -0500

    add error message to image viewing

--- pages/fullsize.php
+++ pages/fullsize.php
@@ -5,32 +5,34 @@ $id = $subpage2;
 $db = Database::getDatabase();
 $row = $db->getRow("SELECT * FROM search WHERE id = '$id'");
 
-$location = WEB_ROOT . $row['location'];
-$medlg = WEB_ROOT . $row['medlg'];
-$mid = WEB_ROOT . $row['mid'];
+if ($row == NULL) {
+  ?>
 
-$trueviews = $row['trueviews'] + 1;
-$views = $row['views'] + 0.05;
-$db->query("UPDATE search SET views = '$views', trueviews = '$trueviews' WHERE id = '$id'");
+<p> "<?php echo $id; ?>" is not a valid image id. Please go back to the
+  <a href="<?php echo WEB_ROOT . 'archives/images/'; ?>"> archive image search
+</a> . </p>
 
-$imgThumb = getimagesize($_SERVER['DOCUMENT_ROOT'] . $mid);
-$imgZoom = getimagesize($_SERVER['DOCUMENT_ROOT'] . $medlg);
-$imgThumbW = $imgThumb[0];
-$imgThumbH = $imgThumb[1];
-$imgZoomW = $imgZoom[0];
-$imgZoomH = $imgZoom[1];
+  <?php
+}
 
-?>
+else {
 
-<div id="zoom01" class="zoom">
-  <img src="<?php echo $mid; ?>" />
-</div>
+  $location = WEB_ROOT . $row['location'];
+  $medlg = WEB_ROOT . $row['medlg'];
+  $mid = WEB_ROOT . $row['mid'];
 
-<p> <?php $row['information']; ?> </p>
-    
-<a href="<?php echo $location; ?>"> Click here for full resolution picture (Warning some pictures are VERY large) </a>
+  $trueviews = $row['trueviews'] + 1;
+  $views = $row['views'] + 0.05;
+  $db->query("UPDATE search SET views = '$views', trueviews = '$trueviews' WHERE id = '$id'");
 
-<a href="feedback.php?id=<?php echo $id; ?>"> Click here to request tag update </a>
+  $imgThumb = getimagesize($_SERVER['DOCUMENT_ROOT'] . $mid);
+  $imgZoom = getimagesize($_SERVER['DOCUMENT_ROOT'] . $medlg);
+  $imgThumbW = $imgThumb[0];
+  $imgThumbH = $imgThumb[1];
+  $imgZoomW = $imgZoom[0];
+  $imgZoomH = $imgZoom[1];
+
+  ?>
 
 <script type="text/javascript">
   /*<![CDATA[*/
@@ -45,3 +47,23 @@ $imgZoomH = $imgZoom[1];
   });
   /*]]>*/
 </script>
+
+<div id="image-viewer">
+
+  <div id="zoom01" class="zoom">
+    <img src="<?php echo $mid; ?>" />
+  </div>
+
+  <div id="image-info">
+    <p> <?php $row['information']; ?> </p>
+
+    <a href="<?php echo $location; ?>"> Click here for full resolution picture (Warning some pictures are VERY large) </a>
+
+    <a href="feedback.php?id=<?php echo $id; ?>"> Click here to request tag update </a>
+  </div>
+
+</div>
+
+  <?php
+  }
+?>
