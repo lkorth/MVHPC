c7ed99476b5b9e4aa158ec8879e5a778d55f9c84
mckennapsean <mckennapsean@gmail.com>
2012-05-27 14:13:03 -0500

    implement jquery-gzoom for images instead of magic-zoom

--- pages/fullsize.php
+++ pages/fullsize.php
@@ -14,12 +14,19 @@ $db = Database::getDatabase();
 $row = $db->getRow("SELECT * FROM search WHERE id = '$id'");
 
 $location = $row['location'];
-$medlg = $row['medlg'];
-$mid = $row['mid'];
+$medlg = WEB_ROOT . $row['medlg'];
+$mid = WEB_ROOT . $row['mid'];
 
 $trueviews = $row['trueviews'] + 1;
 $views = $row['views'] + 0.05;
 $db->query("UPDATE search SET views = '$views', trueviews = '$trueviews' WHERE id = '$id'");
+
+$imgThumb = getimagesize($_SERVER['DOCUMENT_ROOT'] . $mid);
+$imgZoom = getimagesize($_SERVER['DOCUMENT_ROOT'] . $medlg);
+$imgThumbW = $imgThumb[0];
+$imgThumbH = $imgThumb[1];
+$imgZoomW = $imgZoom[0];
+$imgZoomH = $imgZoom[1];
 
 ?>
 <html>
@@ -31,19 +38,43 @@ $db->query("UPDATE search SET views = '$views', trueviews = '$trueviews' WHERE i
                                 'jquery-lightbox-05-min.js',
                                 'jquery-spellchecker.js',
                                 'jquery-tagsinput.js',
-                                'all-images.js');
-    $headerExtras['css'] = array('jquery-lightbox-05.css', 'jquery-spellchecker.css', 'jquery.autocomplete.css', 'jquery-tagsinput.css');
+                                'all-images.js',
+                                'jquery-mousewheel.js',
+                                'jquery-ui-1820-custom-min.js',
+                                'jquery-gzoom.js',
+//                                'magiczoom.js',
+                                );
+    $headerExtras['css'] = array('jquery-lightbox-05.css', 'jquery-spellchecker.css', 'jquery.autocomplete.css', 'jquery-tagsinput.css', 'jquery-ui-1820-custom.css', 'jquery-gzoom.css');
 ?>
 <?php ob_start(); ?>
     <?php
-    echo "<br>";
-    echo "<a href=\"" . WEB_ROOT . "$medlg\" class=\"MagicZoom\" rel=\"click-to-initialize:true;zoom-position:inner;zoom-fade:true;\"><img src=\"" . WEB_ROOT . "$mid\"/></a>";
-    echo "<p>Click the picture to turn on zooming</p>";
-    echo "<br><br>";
+//    echo "<br>";
+//    echo "<a href=\"" . WEB_ROOT . "$medlg\" class=\"MagicZoom\" rel=\"click-to-initialize:true;zoom-position:inner;zoom-fade:true;\"><img src=\"" . WEB_ROOT . "$mid\"/></a>";
+    echo '<div id="zoom01" class="zoom">';
+    echo "<img src=\"$mid\" />";
+    echo '</div>';
+//    echo "<p>Click the picture to turn on zooming</p>";
+//    echo "<br><br>";
     echo '<p>' . $row['information'] . '</p>';
     echo "<br><br>";
     echo "<a href=\"" . WEB_ROOT . "$location\">Click here for full resolution picture (Warning some pictures are VERY large)</a><br>";
     echo "<a href=\"feedback.php?id=$id\">Click here to request tag update</a>";
+    
+    echo "
+		<script type= \"text/javascript\">
+			/*<![CDATA[*/
+			$(function() {
+				$(\"#zoom01\").gzoom({
+						sW: $imgThumbW,
+						sH: $imgThumbH,
+						lW: $imgZoomW,
+						lH: $imgZoomH,
+						lighbox : true
+				});
+		  });
+			/*]]>*/
+		</script>
+		";
     ?>
 <?php
     $content = ob_get_clean();
