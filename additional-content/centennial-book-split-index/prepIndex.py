a1d833f19e2df28e291de7f5494504f7106ad34e
mckennapsean <mckennapsean@gmail.com>
2012-05-29 10:32:23 -0500

    complete Python script for centennial index processing

--- additional-content/centennial-book-split-index/prepIndex.py
+++ additional-content/centennial-book-split-index/prepIndex.py
@@ -1,12 +1,15 @@
 # This file was created by Sean McKenna to convert values
 # of an index into a CSV file for parsing into a database
+# it uses page conversion from an external CSV file
 
 # Defines the CSV data filenames
 inFile = "index.txt"
+parseFile = "centennial-book-pages.csv"
 outFile = "index.csv"
 
 # Necessary imports
 import re
+import csv
 
 # Several regular expressions
 # top or 'p' is what is used here to get pages
@@ -20,10 +23,22 @@ p3 = re.compile('[^0-9\W,]{0,1}([0-9]){1,3}[^0-9\W,]{0,1}')
 input = open(inFile, 'rU')
 reader = input.readlines()
 
+# Grab the CSV file for conversion
+parse = open(parseFile, 'rU')
+parser = csv.reader(parse)
+
 # Prep output file/s
 output = open(outFile, 'wb')
 
-# Process CSV file, row-by-row
+# Keep track of errors
+errors = 0
+
+# Turn parser into a dictionary
+parseTree = {}
+for row in parser:
+  parseTree[row[0]] = row[1]
+
+# Process original index file, row-by-row
 for row in reader:
 
   # strip out ending whitespace
@@ -52,8 +67,15 @@ for row in reader:
         row = row.strip()
       index = row
 
-    # finalize index and initialize pages var
-    index += ';'
+    # null value for no index given
+    else:
+      index = 'NULL'
+      errors += 1
+
+    # get the first char of index
+    char = index[0].upper()
+
+    # initialize pages var
     pages = ''
 
     # for each page we caught with our regex
@@ -63,18 +85,34 @@ for row in reader:
       m2 = m2.replace(',', '')
       m2 = m2.strip()
 
-      # create a CSV list
-      pages += m2 + ','
+      # check parser file for page key and set value
+      if parseTree.has_key(m2):
+        page = parseTree[m2]
+
+      # if not found, print error and NULL value
+      else:
+        page = 'NULL(' + m2 + ')'
+        errors += 1
+
+      # create a CSV list of pages
+      pages += page + ','
 
   # remove final comma, add new line
   pages = pages[0:-1] + '\n'
 
   # combine index and pages and print to file
-  line = index + pages
+  line = char + ';' + index + ';' + pages
   output.write(line)
  
 # Close all files
 input.close()
+parse.close()
 output.close()
 
-print 'Processing of the index complete!'
+# If no errors, print a success message
+if errors == 0:
+  print 'Processing of the index complete!'
+
+# Otherwise, print how many errors were found
+else:
+  print 'There were ' + str(errors) + ' errors while processing the index.'
