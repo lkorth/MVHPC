de585c619dd5edd4623671381e68a516b202efc7
Adam Reed <areed12@cornellcollege.edu>
2012-05-29 15:26:28 -0500

    Styled contact_form.

--- pages/contact-form.php
+++ pages/contact-form.php
@@ -10,11 +10,11 @@ if (!empty($_POST['email']) || !empty($_POST['subject']) || !empty($_POST['messa
     $message = $_POST['message'];
 
     if (!valid_email($email, true))
-        $Error->add('email', "Please enter a valid email address.");
+        $Error->add('email', "<p class='body_text'>Please enter a valid email address.</p>");
     else if (strlen($subject) === 0)
-        $Error->add('subject', "Please enter a subject.");
+        $Error->add('subject', "<p class='body_text'>Please enter a subject.</p>");
     else if (strlen($message) === 0)
-        $Error->add('message', "Please enter a message.");
+        $Error->add('message', "<p class='body_text'>Please enter a message.</p>");
     else {
         send_html_mail('lkorth12@cornellcollege.edu', $subject, $message, $email);
         $success = true;
@@ -23,21 +23,20 @@ if (!empty($_POST['email']) || !empty($_POST['subject']) || !empty($_POST['messa
 
 ob_start();
 ?>
+<h1 class="ribbon">Contact Us</h1>
+
 <?php
 if (isset($success) && $success == true) {
-    echo '<h2>Your request has been successfully submitted, we will contact you soon.</h2>';
+    echo "<p class='body_text'>Your request has been successfully submitted, we will contact you soon.</p>";
 } else {
     ?>
-    <br>
     <?php echo $Error; ?>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="post">
-        <br><p>Your Email Address: <input name="email" type="text" /></p>
-        <p>Subject: <input name="subject" type="text" /><p/>
-        <p>Message: <textarea name="message" rows="30" cols="65"></textarea></p>
-        <br><br>
-        <input type="submit" value="Submit"/>
+        <label for="email" class="body_text">Your Email Address:</label> <input name="email" type="text" />
+        <label for="subject" class="body_text">Subject:</label> <input name="subject" type="text" />
+        <label for="message" class="body_text">Message:</label> <textarea name="message" rows="30" cols="65"></textarea>
+        <input class="button" type="submit" value="Submit"/>
     </form>
-    <br>
     <?php
 }
 $content = ob_get_clean();
