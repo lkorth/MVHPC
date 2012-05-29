ea148cd67aadf9c139ccc8cd83d0bc58e426ee24
Luke Korth <luke@lukekorth.com>
2012-05-29 10:44:48 -0500

    Clean up page-controller logic

--- pages/page-controller.php
+++ pages/page-controller.php
@@ -3,6 +3,8 @@
 require_once '../includes/master.inc.php';
 require_once '../includes/class.template.php';
 
+$template = new Template();
+
 // get current page
 if (!isset($_GET['page']) || !file_exists($_GET['page'].'.php') ) {
     redirect('../error/404.php');
@@ -27,31 +29,32 @@ if (isset($_GET['subpage3']) && !empty($_GET['subpage3'])) {
     $subpage3 = $_GET['subpage3'];
 }
 
-if ($page == 'archives' && $subpage == 'images') {
-//????
-    if (!$fullsize) {
-//        $subpage3 = $_GET['subpage3'];
-        include 'images.php';
-    } else {
-        include 'fullsize.php';
-    }
-}
-// documents from the archives
-else if ($page == 'archives' && $subpage == 'documents') {
-    // show the Centennial Book index instead
-    if ($subpage2 == 'Centennial-Book-Index') {
+if ($page == 'archives') {
+    if($subpage == 'images'){
+      include 'images.php';
+    } else if($subpage2 == 'Centennial-Book-Index'){
         include 'bookindex.php';
+    }else if($subpage == 'documents'){
+      include 'documents.php';
+    } else if($subpage == 'cemetery') {
+    } else if($subpage == 'eras') {
+    } else {
+        include 'about.php';
     }
-    // pdf viewer, display the page by passing in needed variables
-    else {
-        include 'documents.php';
-//        generatePage();
+} else if($page == 'map'){
+    include 'map.php';
+} else if($page == 'making-history'){
+    include 'making-history.php';
+} else if($page == 'about'){
+    if($subpage == 'design-review'){
+        include 'design-review.php';
+    } else if($subpage == 'links'){
+    } else {
+        include 'about.php';
     }
-
-    // pdf viewer, display the page by passing in needed variables
-} else if ($page == 'about' && $subpage == 'design-review') {
-//    generatePage();
 } else {
-    include($page . '.php');
+    redirect('../error/404.php');
 }
+
+$template->output();
 ?>
