f4774dd60df23c28e9c40decaf838b61835c4290
Luke Korth <luke@lukekorth.com>
2012-05-30 01:37:41 -0500

    Try to reduce memory footprint of resize

--- backend/resizeScript.php
+++ backend/resizeScript.php
@@ -2,16 +2,16 @@
 
 require_once '../includes/master.inc.php';
 
-$cnt = 0;
-$mydir = dir('../media');
-while (($file = $mydir->read()) !== false && $cnt < 200) {
-    if ($file != "." && $file != "..") {
-        if (strpos($file, '_thumbnail') !== false || strpos($file, '_mid') !== false || strpos($file, '_medlg') !== false) {
+$names = scandir('../media');
+
+foreach($names as $nm) {
+    if ($nm != "." && $nm != "..") {
+        if (strpos($nm, '_thumbnail') !== false || strpos($nm, '_mid') !== false || strpos($nm, '_medlg') !== false) {
             //do nothing
         } else {
             //read in full size and thumbnail to 224x224
-            $info = pathinfo($file);
-            $file_name = basename($file, '.' . $info['extension']); //file name with no extention
+            $info = pathinfo($nm);
+            $file_name = basename($nm, '.' . $info['extension']); //file name with no extention
             $file_ext = $info['extension']; //outputs the file extension
 
             $full = "../media/" . $file_name . '.' . $file_ext;
@@ -19,19 +19,21 @@ while (($file = $mydir->read()) !== false && $cnt < 200) {
             $filethumb = "../media/" . $file_name . "_thumbnail" . "." . $file_ext;
 
             list($width, $height) = getimagesize($filethumb);
+			
+			echo $width . ' ' . $height . '<br>';
 
-            if ($width != 210 && $height != 210) {
+            if (!isset($width) || !isset($height) || ($width != 210 && $height != 210)) {
                 $gd = new GD($full);
                 $gd->resize(210, 210);
                 $gd->saveAs($filethumb, $file_ext, 100);
                 $cnt++;
                 echo "Resized: $cnt" . '<br>';
+				unset($gd);
             }
             else {
-                //echo 'Already Done<br>';
+                echo 'Already Done<br>';
             }
         }
     }
 }
-$mydir->close();
 ?>
