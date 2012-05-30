bf1c7b4d763698b2f383d197b6a4a8ceb7047e78
Adam Reed <areed12@cornellcollege.edu>
2012-05-30 00:05:01 -0500

    Removed navbar from construction page.

--- index.php
+++ index.php
@@ -7,6 +7,12 @@ $template = new Template();
 $template->setTitle('MVHPC :: Under Construction');
 ob_start();
 ?>
+<style type="text/css">
+    .navbar {
+        display: none;
+    }
+</style>
+
 <h1 class="ribbon">We'll Be Back Soon!</h1>
 
 <img src="images/under-construction.png" style="margin-top: 100px;" />
