351c818721ed5e4c60cf52a3ca823de345b2e102
mckennapsean <mckennapsean@gmail.com>
2012-05-30 00:28:44 -0500

    small changes to design review

--- pages/design-review.php
+++ pages/design-review.php
@@ -8,36 +8,21 @@ ob_start();
 ?>
 <h1 class='ribbon'>Design Review</h1>
 <p class="body_text">
-  Design review is a process to guide property owners in the proper maintenance
-  and enhancement of their structures and neighborhoods. Design review guidelines
-  are promoted and published by the Commission and all property owners in historic
-  districts must apply for one of the following certificates before making any
-  changes in appearance to their property.
+  Design review is a process to guide property owners in the proper maintenance and enhancement of their structures and neighborhoods. Design review guidelines are promoted and published by the Commission and all property owners in historic districts must apply for one of the following certificates before making any changes in appearance to their property.
 </p>
 
 <ul class="body_text">
   <li>
-      Certificate of No Material Effect: Applied for when a property owner that
-      is requesting a building permit demonstrates that there will be no change
-      to the exterior appearance of their building or site which will affect the
-      historic character of the district.
+      <strong> Certificate of No Material Effect </strong>: Applied for when a property owner that is requesting a building permit demonstrates that there will be no change to the exterior appearance of their building or site which will affect the historic character of the district.
   </li>
   <li>
-      Certificate of Appropriateness: Applied for when a property owner requesting
-      a building permit or demolition permit demonstrates that any alterations are
-      in keeping with the Secretary of the Interior's Standards for Rehabilitation
-      or that the improvements will not alter and/or will improve the property's
-      historic appearance.
+      <strong> Certificate of Appropriateness </strong>: Applied for when a property owner requesting a building permit or demolition permit demonstrates that any alterations are in keeping with the Secretary of the Interior's Standards for Rehabilitation or that the improvements will not alter and/or will improve the property's historic appearance.
   </li>
 
 </ul>
 
 <p class="body_text">
-    You will find here all the information needed to secure approval for proposed
-    changes in structures located in Historic Districts. We have also included a helpful
-    guide for making improvements or changes in older homes not included in a Historic
-    District. The commission encourages consultations with us as early a possible in the design
-    process so that any confusions can be clarified before final plans are submitted.
+    You will find here all the information needed to secure approval for proposed changes in structures located in Historic Districts. We have also included a helpful guide for making improvements or changes in older homes not included in a Historic District. The Commission encourages consultations with us as early a possible in the design process so that any confusions can be clarified before final plans are submitted.
 </p>
 <?php
 
