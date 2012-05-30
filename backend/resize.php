c6f7b202f97f90f74863dbda4add213be51b73a2
Luke Korth <luke@lukekorth.com>
2012-05-30 09:11:49 -0500

    Use original resize script

---
+++ backend/resize.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once '../includes/master.inc.php';
+
+$cnt = 0;
+$mydir = dir('../media');
+while (($file = $mydir->read()) !== false && $cnt < 200) {
+    if ($file != "." && $file != "..") {
+        if (strpos($file, '_thumbnail') !== false || strpos($file, '_mid') !== false || strpos($file, '_medlg') !== false) {
+            //do nothing
+        } else {
+            //read in full size and thumbnail to 224x224
+            $info = pathinfo($file);
+            $file_name = basename($file, '.' . $info['extension']); //file name with no extention
+            $file_ext = $info['extension']; //outputs the file extension
+
+            $full = "../media/" . $file_name . '.' . $file_ext;
+            $file_ext = strtolower($file_ext);
+            $filethumb = "../media/" . $file_name . "_thumbnail" . "." . $file_ext;
+
+            list($width, $height) = getimagesize($filethumb);
+
+            if (!isset($width) || !isset($height) || ($width != 210 && $height != 210)) {
+                $gd = new GD($full);
+                $gd->resize(210, 210);
+                $gd->saveAs($filethumb, $file_ext, 100);
+                $cnt++;
+                echo "Resized: $cnt" . '<br>';
+            }
+            else {
+                //echo 'Already Done<br>';
+            }
+        }
+    }
+}
+$mydir->close();
+?>
