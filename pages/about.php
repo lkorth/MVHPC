4cfb38808b3faf34abd15690d9983be3a586cb25
lkorth <luke@lukekorth.com>
2012-08-22 14:19:59 -0700

    Updated members list

--- pages/about.php
+++ pages/about.php
@@ -11,19 +11,16 @@ ob_start();
 
 <h1 class='ribbon'>Members</h1>
 <ul class='fill_list body_text'>
-    <li><span class="fill_list_content">Kay Johansen, Chair</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Richard H. Thomas, Vice Chair</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Edward Sauter</span><span class="bullet">&bull;</span></li>
     <li><span class="fill_list_content">Guy Booth</span><span class="bullet">&bull;</span></li>
     <li><span class="fill_list_content">Hugh Lifson</span><span class="bullet">&bull;</span></li>
     <li><span class="fill_list_content">Leah Rogers</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Pat Westercamp</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Carine Klein</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Paul Robinson</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Pat Westercamp</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Susan Hargus</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Imran Farooqi</span><span class="bullet">&bull;</span></li>
-    <li><span class="fill_list_content">Lawson (Skip) Clark</span></li>
+    <li><span class="fill_list_content">Ed Sauter</span><span class="bullet">&bull;</span></li>
+    <li><span class="fill_list_content">Skip Clark</span><span class="bullet">&bull;</span></li>
+    <li><span class="fill_list_content">Mary Iber</span><span class="bullet">&bull;</span></li>
+    <li><span class="fill_list_content">Sara Kelly</span><span class="bullet">&bull;</span></li>
+    <li><span class="fill_list_content">Pat Westercamp </span><span class="bullet">&bull;</span></li>
+    <li><span class="fill_list_content">Dick Thomas</span><span class="bullet">&bull;</span></li>
+    <li><span class="fill_list_content">Kay Johansen</span></li>
 </ul>
 
 <h1 class='ribbon'>Current Projects</h1>
