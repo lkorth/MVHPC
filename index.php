c591d7101653b2407d7f7d9d18af63a1d757fd05
lkorth <luke@lukekorth.com>
2012-11-09 22:35:10 -0600

    Setup new index file

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
 
+include('includes/master.inc.php');
 require 'vendor/autoload.php';
+
+//class R extends RedBean_Facade {}
+//R::setup('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASSWORD);
+//R::freeze();
 
 $app = new \Slim\Slim(array(
     'view' => new \Slim\Extras\Views\Twig()
@@ -8,9 +13,34 @@ $app = new \Slim\Slim(array(
 $twig = $app->view()->getEnvironment();
 $twig->twigDirectory = './vendor/twig/twig/lib/Twig';
 $twig->twigTemplateDirs = array('./templates');
+$twig->addFilter('max_words', new Twig_Filter_Function('max_words'));
+
+$js = array();
+$js[] = 'jquery-172.js';
+$js[] = 'heights.js';
+
+$css = array();
+$css[] = 'EngraversMT.css';
+$css[] = 'Belleza.css';
+$css[] = 'main.css';
+
+$twig->addGlobal('css', $css);
+//$twig->addGlobal('js', $js);
+$twig->addGlobal('CACHE_BREAK_CSS', CACHE_BREAK_CSS);
+$twig->addGlobal('CACHE_BREAK_JS', CACHE_BREAK_JS);
 
 $app->get('/', function () use ($app) {
-    $app->render('index.twig');
+    global $js;
+
+    $arr = array();
+    $arr['page'] = 'home';
+    $arr['images'] = featuredImages();
+    $arr['post'] = returnPosts('index', 1);
+    $arr['post'] = $arr['post'][0];
+    array_push($js, 'image-script.js');
+    $arr['js'] = $js;
+
+    $app->render('index.twig', $arr);
 });
 
 $app->run();
