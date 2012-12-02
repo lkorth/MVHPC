b49fb4e18e3b034ac3e5ad38f3720f1bbd32c5c6
lkorth <luke@lukekorth.com>
2012-12-01 20:34:24 -0600

    Update minifyer

--- min/config.php
+++ min/config.php
@@ -2,15 +2,21 @@
 /**
  * Configuration for "min", the default application built with the Minify
  * library
- *
+ * 
  * @package Minify
  */
 
 
 /**
  * Allow use of the Minify URI Builder app. Only set this to true while you need it.
- **/
+ */
 $min_enableBuilder = false;
+
+/**
+ * If non-empty, the Builder will be protected with HTTP Digest auth.
+ * The username is "admin".
+ */
+$min_builderPassword = 'admin';
 
 
 /**
@@ -32,7 +38,7 @@ $min_errorLogger = false;
  * /min/f=file1.js send the cookie minDebug=file1.js
  * You can manually enable debugging by appending "&debug" to a URI.
  * E.g. /min/?f=script1.js,script2.js&debug
- *
+ * 
  * In 'debug' mode, Minify combines files with no minification and adds comments
  * to indicate line #s of the original files.
  */
@@ -57,11 +63,11 @@ $min_cachePath = '../cache';
 /**
  * Leave an empty string to use PHP's $_SERVER['DOCUMENT_ROOT'].
  *
- * On some servers, this value may be misconfigured or missing. If so, set this
+ * On some servers, this value may be misconfigured or missing. If so, set this 
  * to your full document root path with no trailing slash.
  * E.g. '/home/accountname/public_html' or 'c:\\xampp\\htdocs'
  *
- * If /min/ is directly inside your document root, just uncomment the
+ * If /min/ is directly inside your document root, just uncomment the 
  * second line. The third line might work on some Apache servers.
  */
 //$min_documentRoot = '';
@@ -70,7 +76,7 @@ $min_documentRoot = substr(__FILE__, 0, -15);
 
 
 /**
- * Cache file locking. Set to false if filesystem is NFS. On at least one
+ * Cache file locking. Set to false if filesystem is NFS. On at least one 
  * NFS system flock-ing attempts stalled PHP for 30 seconds!
  */
 $min_cacheFileLocking = true;
@@ -79,9 +85,9 @@ $min_cacheFileLocking = true;
 /**
  * Combining multiple CSS files can place @import declarations after rules, which
  * is invalid. Minify will attempt to detect when this happens and place a
- * warning comment at the top of the CSS output. To resolve this you can either
- * move the @imports within your CSS files, or enable this option, which will
- * move all @imports to the top of the output. Note that moving @imports could
+ * warning comment at the top of the CSS output. To resolve this you can either 
+ * move the @imports within your CSS files, or enable this option, which will 
+ * move all @imports to the top of the output. Note that moving @imports could 
  * affect CSS values (which is why this option is disabled by default).
  */
 $min_serveOptions['bubbleCssImports'] = false;
@@ -100,15 +106,10 @@ $min_serveOptions['maxAge'] = 3600;
 
 
 /**
- * To use Google's Closure Compiler API (falling back to JSMin on failure),
- * uncomment the following lines:
+ * To use Google's Closure Compiler API to minify Javascript (falling back to JSMin
+ * on failure), uncomment the following line:
  */
-/*function closureCompiler($js) {
-    require_once 'Minify/JS/ClosureCompiler.php';
-    return Minify_JS_ClosureCompiler::minify($js);
-}
-$min_serveOptions['minifiers']['application/x-javascript'] = 'closureCompiler';
-//*/
+//$min_serveOptions['minifiers']['application/x-javascript'] = array('Minify_JS_ClosureCompiler', 'minify');
 
 
 /**
@@ -116,8 +117,8 @@ $min_serveOptions['minifiers']['application/x-javascript'] = 'closureCompiler';
  * particular directories below DOCUMENT_ROOT, set this here.
  * You will still need to include the directory in the
  * f or b GET parameters.
- *
- * // = shortcut for DOCUMENT_ROOT
+ * 
+ * // = shortcut for DOCUMENT_ROOT 
  */
 //$min_serveOptions['minApp']['allowDirs'] = array('//js', '//css');
 
@@ -131,7 +132,7 @@ $min_serveOptions['minApp']['groupsOnly'] = false;
 /**
  * By default, Minify will not minify files with names containing .min or -min
  * before the extension. E.g. myFile.min.js will not be processed by JSMin
- *
+ * 
  * To minify all files, set this option to null. You could also specify your
  * own pattern that is matched against the filename.
  */
@@ -142,8 +143,8 @@ $min_serveOptions['minApp']['noMinPattern'] = '@[-\\.]min\\.(?:js|css)$@i';
  * If you minify CSS files stored in symlink-ed directories, the URI rewriting
  * algorithm can fail. To prevent this, provide an array of link paths to
  * target paths, where the link paths are within the document root.
- *
- * Because paths need to be normalized for this to work, use "//" to substitute
+ * 
+ * Because paths need to be normalized for this to work, use "//" to substitute 
  * the doc root in the link paths (the array keys). E.g.:
  * <code>
  * array('//symlink' => '/real/target/path') // unix
@@ -155,17 +156,17 @@ $min_symlinks = array();
 
 /**
  * If you upload files from Windows to a non-Windows server, Windows may report
- * incorrect mtimes for the files. This may cause Minify to keep serving stale
+ * incorrect mtimes for the files. This may cause Minify to keep serving stale 
  * cache files when source file changes are made too frequently (e.g. more than
  * once an hour).
- *
- * Immediately after modifying and uploading a file, use the touch command to
+ * 
+ * Immediately after modifying and uploading a file, use the touch command to 
  * update the mtime on the server. If the mtime jumps ahead by a number of hours,
- * set this variable to that number. If the mtime moves back, this should not be
+ * set this variable to that number. If the mtime moves back, this should not be 
  * needed.
  *
- * In the Windows SFTP client WinSCP, there's an option that may fix this
- * issue without changing the variable below. Under login > environment,
+ * In the Windows SFTP client WinSCP, there's an option that may fix this 
+ * issue without changing the variable below. Under login > environment, 
  * select the option "Adjust remote timestamp with DST".
  * @link http://winscp.net/eng/docs/ui_login_environment#daylight_saving_time
  */
@@ -173,7 +174,7 @@ $min_uploaderHoursBehind = 0;
 
 
 /**
- * Path to Minify's lib folder. If you happen to move it, change
+ * Path to Minify's lib folder. If you happen to move it, change 
  * this accordingly.
  */
 $min_libPath = dirname(__FILE__) . '/lib';
