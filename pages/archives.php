e8916ec86baca75538eb9fe46cbd63cd2e6ae942
lkorth <luke@lukekorth.com>
2012-12-02 13:44:23 -0600

    Add route and template for searching images

--- pages/archives.php
+++ pages/archives.php
@@ -4,8 +4,14 @@ $app->get('/archives', function () use ($app) {
     $app->render('archives.twig', array('page' => 'archives'));
 });
 
+$app->get('/archives/images(/:terms)', function ($terms = null) use ($app) {
+    $arr = array();
+    $arr['page'] = 'archives';
+    $arr['terms'] = $terms;
 
 
 
 
+    $app->render('archives.images.twig', $arr);
+});
 
