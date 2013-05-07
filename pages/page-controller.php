3165d7dced4eb7bb4886460c9951d4dacdc2806f
lkorth <luke@lukekorth.com>
2013-05-06 22:44:57 -0500

    Delete old code

--- pages/page-controller.php
+++ pages/page-controller.php
@@ -1,10 +1,4 @@
 <?php
-// include the framework and templating
-require_once '../includes/master.inc.php';
-require_once '../includes/class.template.php';
-
-// create the template
-$template = new Template();
 
 // allow for code to be inserted into the header
 // custom HTML, javascript, CSS (in this order)
@@ -13,30 +7,6 @@ $template = new Template();
 $headerCustom = NULL;
 $headerCSS = array();
 $headerJS = array();
-
-// get current page
-if (!isset($_GET['page']) || !file_exists($_GET['page'].'.php') ) {
-    redirect('../error/error.php?type=404');
-}
-$page = $_GET['page'];
-
-// get the sub-page, if applicable
-$subpage = NULL;
-if (isset($_GET['subpage']) && !empty($_GET['subpage'])) {
-    $subpage = $_GET['subpage'];
-}
-
-// get the sub-sub-page, if applicable
-$subpage2 = NULL;
-if (isset($_GET['subpage2']) && !empty($_GET['subpage2'])) {
-    $subpage2 = $_GET['subpage2'];
-}
-
-// get the sub-sub-sub-page, if applicable
-$subpage3 = NULL;
-if (isset($_GET['subpage3']) && !empty($_GET['subpage3'])) {
-    $subpage3 = $_GET['subpage3'];
-}
 
 if ($page == 'archives') {
     if($subpage == 'images'){
@@ -91,6 +61,3 @@ $headerExtras = array(
 );
 $template->setHeaderExtras($headerExtras);
 
-// output the template
-$template->output();
-?>
